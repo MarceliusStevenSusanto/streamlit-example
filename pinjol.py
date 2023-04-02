43543c96969fc0d10288b6abcd1691701ad5ee8e
Marcelius steven Susanto <124881983+MarceliusStevenSusanto@users.noreply.github.com>
2023-04-02 12:24:34 +0700

    Update pinjol.py

--- pinjol.py
+++ pinjol.py
@@ -1,5 +1,19 @@
 import streamlit as st
 
+# Set page configuration
+st.set_page_config(page_title="Aplikasi Kalkulator Pinjol", page_icon=":money_with_wings:", layout="wide")
+
+# Set theme configuration
+COLOR_THEME = "#99ccff"
+st.markdown(f"""
+<style>
+    .reportview-container {{
+        background-color: {COLOR_THEME};
+    }}
+</style>
+""", unsafe_allow_html=True)
+
+
 def total_pinjaman(gaji_bulanan, jumlah_tanggungan, durasi_pinjaman):
     """
     Fungsi ini bertujuan untuk menghitung total uang yang dapat dipinjam,
@@ -57,7 +71,7 @@ def total_pinjaman(gaji_bulanan, jumlah_tanggungan, durasi_pinjaman):
     pinjaman = gaji_bulanan * durasi_pinjaman * tingkat_bunga
     return pinjaman
 
-st.title('Kalkulator Pinjaman')
+st.title('Kalkulator Pinjol')
 
 with st.form('input_form'):
     st.write('Silahkan masukkan nilai-nilai berikut:')
